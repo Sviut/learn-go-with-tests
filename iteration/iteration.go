0038d35fcc8ae362937bbf30624ade65f3eeb9d6
Sviut <jeleznov88@gmail.com>
2025-01-31 21:21:53 +0300

    iteration example

---
+++ iteration/iteration.go
@@ -0,0 +1,13 @@
+package iteration
+
+import "strings"
+
+func Repeat(char string, repeatCount int) string {
+	var repeated strings.Builder
+
+	for i := 0; i < repeatCount; i++ {
+		repeated.WriteString(char)
+	}
+
+	return repeated.String()
+}
